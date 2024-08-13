d6c14a8ac68dd2c58501bff2f9bb8c48a947d88b
PratikVMarathe <86453383+PratikVMarathe@users.noreply.github.com>
2024-08-13 14:45:34 +0530

    Deleting Item By Clicking

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,38 +1,51 @@
 import React, { useState } from "react";
+import ToDoItem from "./ToDoItem";
 
 function App() {
-  const [item, setItem] = useState("");
-  const [newItem, setNewItem] = useState([]);
+  const [inputText, setInputText] = useState("");
+  const [items, setItems] = useState([]);
 
   function handleChange(event) {
-    setItem(event.target.value);
+    const newValue = event.target.value;
+    setInputText(newValue);
   }
 
-  function handleClick() {
-    setNewItem((prevItems) => [...prevItems, item]);
-    setItem("");
+  function addItem() {
+    setItems((prevItems) => {
+      return [...prevItems, inputText];
+    });
+    setInputText("");
   }
+
+  const deleteItem = (id) => {
+    setItems((prevItems) => {
+      return prevItems.filter((item, index) => {
+        return index !== id;
+      });
+    });
+  };
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input
-          name="message"
-          value={item}
-          onChange={handleChange}
-          type="text"
-        />
-        <button onClick={handleClick}>
+        <input onChange={handleChange} type="text" value={inputText} />
+        <button onClick={addItem}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          {newItem.map((item) => {
-            return <li>{item}</li>;
-          })}
+          {items.map((todoItem, index) => (
+            <ToDoItem
+              key={index}
+              id={index}
+              onCheckd={deleteItem}
+              text={todoItem}
+            />
+          ))}
         </ul>
       </div>
     </div>
